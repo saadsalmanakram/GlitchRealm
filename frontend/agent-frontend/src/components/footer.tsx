1e61efdb262acf1bb3a6629ee88584e8975ba01d
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-26 22:53:44 +0500

    update

--- frontend/agent-frontend/src/components/footer.tsx
+++ frontend/agent-frontend/src/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 text-center border-t border-gray-700">
       <p className="text-gray-400 text-sm">
-        &copy; 2025 Artilence. All Rights Reserved.
+        &copy; 2025 Saad Salman. All Rights Reserved.
       </p>
     </footer>
   );
