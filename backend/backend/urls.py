ad8404d595cd537830326ab574cece519b1b180a
Saad Salman <saadsalmanakram1@gmail.com>
2025-01-26 13:01:22 +0500

    update

--- backend/backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from chat.views import ChatViewSet
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/chat/', ChatViewSet.as_view(), name='chat'),
-    path('api/', include('rest_framework.urls', namespace='rest_framework')),
-]
